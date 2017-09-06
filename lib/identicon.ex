99d1c515c694269a3ecb00f2b19f76d9480bc342
Ezols <Edvards.ozols@gmail.com>
2017-09-06 11:10:06 +0300

    Grid builded

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,7 +7,36 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
 
+  end
+
+  def build_grid(image) do
+    
+    # Get access to hex content
+    %Identicon.Image{hex: hex} = image
+    # Pass hex to pipes
+    grid = hex
+      # create a row for grid of 3 elements
+      |> Enum.chunk(3)
+      # Map all elements passed in, and mirror rows, creating 5 element grid
+      |> Enum.map(&mirror_row/1)
+      # Put all stuff in one list
+      |> List.flatten
+      # Turns list elemts into tuple which consists two elements,
+      # first elemnt is element from list, second element is index in the list
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+
+  end
+
+  def mirror_row(row) do
+    # [145, 26, 200]
+    [first, second | _tail] = row
+
+    # [145, 26, 200, 46, 145]
+    row ++ [second, first]
   end
 
   def pick_color(image) do
